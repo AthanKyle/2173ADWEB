d4d4e5c39ee688064439b1a582e2ab9dedc8c45b
AthanKyle <123538598+AthanKyle@users.noreply.github.com>
2024-01-11 09:11:20 +0800

    Add files via upload
    1st Act 1/11/24

---
+++ Lab Demo Data Binding Maglalang/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'this string is interpalated';
+  clientName = 'Kyle';
+  appliedCSS = 'green';
+  notAppliedCSS = false;
+  myColor = 'red';
+
+//Event Binding
+  clickCount=0;
+  clickMe() {
+  this.clickCount++;
+  }
+}
+
+
